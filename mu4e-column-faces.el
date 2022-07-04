c5938a4f4ed23cf543f73d24e0a6f53272bdcd4a
Alexander Miller <alexanderm@web.de>
2022-07-04 21:20:47 +0200

    Update for mu 1.8.
    Fixes #6.

--- mu4e-column-faces.el
+++ mu4e-column-faces.el
@@ -1,11 +1,11 @@
 ;;; mu4e-column-faces.el --- Faces for individual mu4e columns -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021 Alexander Miller
+;; Copyright (C) 2022 Alexander Miller
 
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Package-Requires: ((emacs "25.3"))
 ;; Homepage: https://github.com/Alexander-Miller/mu4e-column-faces
-;; Version: 1.1
+;; Version: 1.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -110,7 +110,8 @@
 Value must be a function that takes two arguments:
 - the column
 - the mu4e message object"
-  :group 'mu4e-column-faces)
+  :group 'mu4e-column-faces
+  :type 'function)
 
 (defcustom mu4e-column-faces-adjust-face nil
   "Function to optionally further adjust mu4e's column faces.
@@ -121,22 +122,98 @@ Value must be a function that takes 2 arguments:
 - the column
 - the column's value
 - the mu4e message object"
-  :group 'mu4e-column-faces)
+  :group 'mu4e-column-faces
+  :type 'function)
 
-(defun mu4e-column-faces--header-handler (msg &optional point)
+(defun mu4e-column-faces-~headers-append-handler (msglst)
   "Entry point for the mu4e overrides.
-Overrides `mu4e~headers-header-handler' out of necessity because all the
+Overrides `mu4e~headers-append-handler' out of necessity because all the
 functions that actually do need changing are inlined.
 
-The only change is for MSG to be passed to
-`mu4e-column-faces--msg-header-line' to create a line with column faces,
-POINT remains untouched."
+The only change is for MSGLST to be passed to
+`mu4e-column-faces--msg-header-line'instead of `mu4e~headers-insert-header'."
   (when (buffer-live-p (mu4e-get-headers-buffer))
     (with-current-buffer (mu4e-get-headers-buffer)
-      (let ((line (mu4e-column-faces--msg-header-line msg)))
-        (when line
-          (mu4e~headers-add-header line (mu4e-message-field msg :docid)
-                                   point msg))))))
+      (save-excursion
+	    (let ((inhibit-read-only t))
+	      (seq-do
+	       (lambda (msg)
+	         (mu4e-column-faces--insert-header msg (point-max)))
+	       msglst))))))
+
+(defun mu4e-column-faces--update-handler (msg is-move maybe-view)
+  "Entry point for the mu4e overrides.
+Overrides `mu4e~headers-update-handler' out of necessity because all the
+functions that actually do need changing are inlined.
+
+Full function body is copied verbatim, the only change is that MSG is passed to
+`mu4e-column-faces--insert-header' instead of `mu4e~headers-insert-header'.
+IS-MOVE and MAYBE-VIEW remain untouched."
+  (when (buffer-live-p (mu4e-get-headers-buffer))
+    (with-current-buffer (mu4e-get-headers-buffer)
+      (let* ((docid (mu4e-message-field msg :docid))
+             (initial-message-at-point (mu4e~headers-docid-at-point))
+             (initial-column (current-column))
+	         (inhibit-read-only t)
+             (point (mu4e~headers-docid-pos docid))
+             (markinfo (gethash docid mu4e--mark-map)))
+        (when point ;; is the message present in this list?
+
+          ;; if it's marked, unmark it now
+          (when (mu4e-mark-docid-marked-p docid)
+            (mu4e-mark-set 'unmark))
+
+          ;; re-use the thread info from the old one; this is needed because
+          ;; *update* messages don't have thread info by themselves (unlike
+          ;; search results)
+          ;; since we still have the search results, re-use
+          ;; those
+          (plist-put msg :meta
+                     (mu4e~headers-field-for-docid docid :meta))
+
+          ;; first, remove the old one (otherwise, we'd have two headers with
+          ;; the same docid...
+          (mu4e~headers-remove-header docid t)
+
+          ;; if we're actually viewing this message (in mu4e-view mode), we
+          ;; update it; that way, the flags can be updated, as well as the path
+          ;; (which is useful for viewing the raw message)
+          (when (and maybe-view (mu4e~headers-view-this-message-p docid))
+            (save-excursion (mu4e-view msg)))
+          ;; now, if this update was about *moving* a message, we don't show it
+          ;; anymore (of course, we cannot be sure if the message really no
+          ;; longer matches the query, but this seem a good heuristic.  if it
+          ;; was only a flag-change, show the message with its updated flags.
+          (unless is-move
+	        (save-excursion
+	          (mu4e-column-faces--insert-header msg point)))
+
+          ;; restore the mark, if any. See #2076.
+          (when (and markinfo (mu4e~headers-goto-docid docid))
+            (mu4e-mark-at-point (car markinfo) (cdr markinfo)))
+
+          (if (and initial-message-at-point
+                   (mu4e~headers-goto-docid initial-message-at-point))
+              (progn
+                (move-to-column initial-column)
+                (mu4e~headers-highlight initial-message-at-point))
+            ;; attempt to highlight the corresponding line and make it visible
+            (mu4e~headers-highlight docid))
+          (run-hooks 'mu4e-message-changed-hook))))))
+
+(define-inline mu4e-column-faces--insert-header (msg pos)
+  "Insert a header for MSG at point POS."
+  (inline-letevals (msg pos)
+    (inline-quote
+     (when-let ((line (mu4e-column-faces--msg-header-line ,msg))
+	            (docid (plist-get ,msg :docid)))
+       (goto-char ,pos)
+       (insert
+        (propertize
+         (concat
+          (mu4e~headers-docid-cookie docid)
+          mu4e--mark-fringe line "\n")
+         'docid docid 'msg ,msg))))))
 
 (define-inline mu4e-column-faces--msg-header-line (msg)
   "Create a propertized header for the given MSG.
@@ -206,21 +283,25 @@ the message flags in included in `mu4e-column-faces--apply-face'."
   "Global minor mode for individual column faces in mu4e's email overview.
 The view must be refreshed with `mu4e-headers-rerun-search' for the changes to
 take effect.
-Requires at least mu4e v1.6.0."
+Requires at least mu4e v1.8.0."
   :init-value nil
   :global     t
   :lighter    nil
   :group      'mu4e-column-faces
   (if mu4e-column-faces-mode
       (progn
-        (unless (version<= "1.6.0" mu4e-mu-version)
+        (unless (version<= "1.8.0" mu4e-mu-version)
           (user-error
-           "Mu4e-column-faces-mode requires at least mu4e 1.6.0, current version is %s"
+           "Mu4e-column-faces-mode requires at least mu4e 1.8.0, current version is %s"
            mu4e-mu-version))
-        (advice-add #'mu4e~headers-header-handler
-                    :override #'mu4e-column-faces--header-handler))
-    (advice-remove #'mu4e~headers-header-handler
-                   #'mu4e-column-faces--header-handler)))
+        (advice-add #'mu4e~headers-append-handler
+                    :override #'mu4e-column-faces-~headers-append-handler)
+        (advice-add #'mu4e~headers-update-handler
+                    :override #'mu4e-column-faces--update-handler))
+    (advice-remove #'mu4e~headers-append-handler
+                   #'mu4e-column-faces-~headers-append-handler)
+    (advice-add #'mu4e~headers-update-handler
+                #'mu4e-column-faces--update-handler)))
 
 (provide 'mu4e-column-faces)
 
